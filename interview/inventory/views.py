75ade07544337ba592facb13c90a5ddca4b0a88b
davidjonathansheffield <108345864+davidjonathansheffield@users.noreply.github.com>
2025-01-27 16:32:41 -0500

    Adds pagination to InventoryListCreateView.

--- interview/inventory/views.py
+++ interview/inventory/views.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
@@ -7,10 +8,37 @@ from interview.inventory.schemas import InventoryMetaData
 from interview.inventory.serializers import InventoryLanguageSerializer, InventorySerializer, InventoryTagSerializer, InventoryTypeSerializer
 
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class InventoryListCreateView(APIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
-    
+    pagination_class = StandardResultsSetPagination
+
+    def _paginate_queryset(self, queryset):
+        if self.pagination_class is None:
+            return None
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(queryset, self.request)
+        if page is not None:
+            return page
+        return None
+
+    def _get_paginated_response(self, data):
+        """
+        Optionally, this could also be abstracted into its own file called pagination to apply this to different models
+        rather than just Inventory.  Or onto a base class that all views that wish to paginate can inherit from.  This
+        seems like overkill at the current moment to efficiently tackle this task (and the rest in the time limit) but
+        it is a great option moving forward in a real world scenario.
+        """
+        assert self.pagination_class is not None
+        paginator = self.pagination_class()
+        return paginator.get_paginated_response(data)
+
     def post(self, request: Request, *args, **kwargs) -> Response:
         try:
             metadata = InventoryMetaData(**request.data['metadata'])
@@ -27,13 +55,18 @@ class InventoryListCreateView(APIView):
         return Response(serializer.data, status=201)
     
     def get(self, request: Request, *args, **kwargs) -> Response:
+        queryset = self.get_queryset()
+        page = self._paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.serializer_class(page, many=True)
+            return self._get_paginated_response(serializer.data)
+
         serializer = self.serializer_class(self.get_queryset(), many=True)
-        
         return Response(serializer.data, status=200)
     
     def get_queryset(self):
         return self.queryset.all()
-    
+
 
 class InventoryRetrieveUpdateDestroyView(APIView):
     queryset = Inventory.objects.all()
