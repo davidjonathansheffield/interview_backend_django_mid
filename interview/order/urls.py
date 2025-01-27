9ba0118161118ec85bdf70d1480df9b3b55874c1
davidjonathansheffield <108345864+davidjonathansheffield@users.noreply.github.com>
2025-01-27 16:42:59 -0500

    Adds orders by tag view.

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListByTagView, OrderListCreateView, OrderTagListCreateView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('by-tag/<str:tag>/', OrderListByTagView.as_view(), name='order-list-by-tag'),
 ]
