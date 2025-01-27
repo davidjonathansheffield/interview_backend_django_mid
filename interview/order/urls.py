9b03ce135f9fc863a710e8347053310d76f4b510
davidjonathansheffield <108345864+davidjonathansheffield@users.noreply.github.com>
2025-01-27 16:50:14 -0500

    Adds Tags of Order functionality.

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, TagsOnOrdersView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
+    path('tags/by-order/<int:pk>/', TagsOnOrdersView.as_view(), name='order-tag-list'),
     path('', OrderListCreateView.as_view(), name='order-list'),
 
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +14,14 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class TagsOnOrdersView(generics.ListAPIView):
+    serializer_class = OrderTagSerializer
+
+    def get_queryset(self):
+        """ Returns all tags on an order."""
+        try:
+            return Order.objects.get(pk=self.kwargs.get('pk')).tags.all()
+        except Order.DoesNotExist:
+            raise ValidationError('Order does not exist')
